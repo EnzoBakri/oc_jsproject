f84a6f4a84f2a8a97fed08baa44fee26e93d3211
Enzo Bakri <bakrienzo@gmail.com>
2023-01-04 23:22:08 +0100

    ETAPE 1.1 WIP

---
+++ FrontEnd/index.js
@@ -0,0 +1,34 @@
+// Récupération des travaux depuis le fichier JSON
+const reponse = await fetch('../Backend/works.json');
+const works = await reponse.json();
+
+// Création des balises
+const article = works[0];
+
+for (let i = 0; i < works.length; i++) {
+
+    // Récupération de l'élément du DOM qui accueillera les travaux
+    const divGallery = document.querySelector(".gallery");
+
+    // Création d'une balise dédiée à un travail
+    const workElement = document.createElement("figure");
+
+    // On créé l'élément img.
+    const  imageElement = document.createElement("img");
+
+    // On accède à l'indice i de la liste works pour configurer la source de l'image.
+    imageElement.src = works[i].image;
+    
+    // Idem pour le nom
+    const  nomElement = document.createElement("figcaption");
+    nomElement.innerText = works[i].title;
+    
+    // On rattache la balise figure à la div Gallery
+    divGallery.appendChild(workElement);
+
+    // On rattache l'image à workElement (la balise figure)
+    workElement.appendChild(imageElement);
+
+    // Idem pour le nom
+    workElement.appendChild(nomElement);
+}
