9c8883bc333367239449028fd84f7ca8fa538bee
Enzo Bakri <bakrienzo@gmail.com>
2023-01-07 14:06:40 +0100

    ETAPE 1.2 OK

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,35 +1,64 @@
-// Récupération des travaux depuis le fichier JSON
+// Récupération des projets depuis le fichier JSON
 const reponse = await fetch('http://localhost:5678/api/works');
 const works = await reponse.json();
 
-// Création des balises
-const article = works[0];
+function generateWorks(works) {
+    for (let i = 0; i < works.length; i++) {
 
-for (let i = 0; i < works.length; i++) {
-
-    // Récupération de l'élément du DOM qui accueillera les travaux
-    const divGallery = document.querySelector(".gallery");
-
-    // Création d'une balise dédiée à un travail
-    const workElement = document.createElement("figure");
-
-    // On créé l'élément img.
-    const  imageElement = document.createElement("img");
-
-    // On accède à l'indice i de la liste works pour configurer la source de l'image.
-    imageElement.src = works[i].imageUrl;
-    imageElement.crossOrigin = "Anonymous";
-    
-    // Idem pour le nom
-    const  nomElement = document.createElement("figcaption");
-    nomElement.innerText = works[i].title;
-    
-    // On rattache la balise figure à la div Gallery
-    divGallery.appendChild(workElement);
-
-    // On rattache l'image à workElement (la balise figure)
-    workElement.appendChild(imageElement);
-
-    // Idem pour le nom
-    workElement.appendChild(nomElement);
+        const article = works[i];
+        // Récupération de l'élément du DOM qui accueillera les travaux
+        const divGallery = document.querySelector(".gallery");
+        // Création d'une balise dédiée à un projet
+        const workElement = document.createElement("figure");
+        // Création des balises
+        const  imageElement = document.createElement("img");
+        imageElement.src = article.imageUrl;
+        imageElement.crossOrigin = "Anonymous";
+        const  nomElement = document.createElement("figcaption");
+        nomElement.innerText = article.title;
+        // On rattache les éléments à leur parent
+        divGallery.appendChild(workElement);
+        workElement.appendChild(imageElement);
+        workElement.appendChild(nomElement);
+    }
 }
+
+generateWorks(works);
+
+// Gestion des boutons
+const allButton = document.querySelector("#all");
+
+allButton.addEventListener("click", function() {
+    document.querySelector(".gallery").innerHTML = "";
+    generateWorks(works);
+});
+
+const objectsButton = document.querySelector("#objects");
+
+objectsButton.addEventListener("click", function() {
+    const filteredWorks = works.filter(function(work) {
+        return work.categoryId === 1;
+    });
+    document.querySelector(".gallery").innerHTML = "";
+    generateWorks(filteredWorks);
+});
+
+const apartmentButton = document.querySelector("#apartment");
+
+apartmentButton.addEventListener("click", function() {
+    const filteredWorks = works.filter(function(work) {
+        return work.categoryId === 2;
+    });
+    document.querySelector(".gallery").innerHTML = "";
+    generateWorks(filteredWorks);
+});
+
+const hotelRestaurantButton = document.querySelector("#hotel-restaurant");
+
+hotelRestaurantButton.addEventListener("click", function() {
+    const filteredWorks = works.filter(function(work) {
+        return work.categoryId === 3;
+    });
+    document.querySelector(".gallery").innerHTML = "";
+    generateWorks(filteredWorks);
+});
